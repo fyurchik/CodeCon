0df2f8beadc82763eeea33b84d19e29a3c245656
nicjkkd <kolya.lukianov.2018@gmail.com>
2024-04-13 14:07:16 +0300

    feat: login page

--- client/src/routes/auth/register.tsx
+++ client/src/routes/auth/register.tsx
@@ -24,7 +24,7 @@ const Page = () => {
 
     // const signUpHandler = useSignUp();
 
-    const onSubmit = (values: Schema) => {
+    const onSubmit = (values: RegisterSchema) => {
         // signUpHandler.mutate({ email: values.email, password: values.password });
     };
 
