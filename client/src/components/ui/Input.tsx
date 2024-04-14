d033082551c94260c97680c1710c55ba8c69f435
nicjkkd <kolya.lukianov.2018@gmail.com>
2024-04-14 03:07:45 +0300

    feat: login page updated styles

--- client/src/components/ui/Input.tsx
+++ client/src/components/ui/Input.tsx
@@ -7,7 +7,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(({ className, type, ...pr
     <input
         type={type}
         className={cn(
-            "flex h-10 w-full rounded-md border border-border bg-background px-3 py-2 text-sm ring-offset-background transition file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-accent-foreground focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
+            "file:border-1 flex h-16 w-full gap-2.5 rounded-3xl border border-[#D4D4D4] border-border bg-[#F5F5F5] bg-background px-3 py-2 text-sm ring-offset-background transition file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-accent-foreground focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
             className
         )}
         ref={ref}
