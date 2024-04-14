e5c8c12a4e36382a77366ac445771cbc0b475198
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-04-14 13:45:47 +0300

    removed dark theme

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import ThemeContextProvider from "./context/Theme";
 import UserContextProvider from "./context/User";
 import "./index.css";
 
@@ -21,11 +20,9 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
-            <ThemeContextProvider>
-                <UserContextProvider>
-                    <RouterProvider router={router} />
-                </UserContextProvider>
-            </ThemeContextProvider>
+            <UserContextProvider>
+                <RouterProvider router={router} />
+            </UserContextProvider>
         </QueryClientProvider>
     </React.StrictMode>
 );
